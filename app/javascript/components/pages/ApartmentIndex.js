aa7ce3a057eca8ef86326d00be183b2b5534b02e
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-04-14 09:25:45 -0700

    Adds additional test and reworded testing focus

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import {Card, CardText, CardTitle, CardSubtitle, CardBody, Button} from  'reactstrap'
 
-const ApartmentIndex = () => {
-  return (
-    <div>ApartmentIndex</div>
+const ApartmentIndex = ({ apartments }) => {
+  return ( 
+    <>
+    <h1>Apartment Listings</h1>
+    {apartments?.map((value,index) => {
+      return (
+    <Card style={{ width: '18rem' }} key={index}>
+      <img 
+      src={value.image} 
+      alt="App Image"  
+      />
+    <CardBody>
+    <CardTitle tag="h5">
+      Address: {value.address}
+    </CardTitle>
+    <CardSubtitle className="mb-2 text-muted" tag="h6">
+      Price: ${value.price}
+    </CardSubtitle>
+    <CardSubtitle className="mb-2 text-muted" tag="h6">
+      Bedrooms/s: {value.bedrooms}
+    </CardSubtitle>
+        <a href={`/apartmentshow/${value.id}`}>More Info</a>
+    </CardBody>
+</Card>
+
+  )
+}
+  )}
+  </>
   )
 }
 
