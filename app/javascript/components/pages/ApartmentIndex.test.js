4b80ab5ef4701349a02667059166a60574fc5482
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2023-04-14 10:22:35 -0700

    Added additional specfic test to our index

--- app/javascript/components/pages/ApartmentIndex.test.js
+++ app/javascript/components/pages/ApartmentIndex.test.js
@@ -4,16 +4,22 @@ import {render, screen} from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { BrowserRouter } from "react-router-dom"
 import ApartmentIndex from "./ApartmentIndex"
+import { Card } from "reactstrap"
 
 describe("<ApartmentIndex />", () => {
-    it("renders without crashing", () => {})
-    it("renders Index page", () => {
-      render(
-        <BrowserRouter>
-          <ApartmentIndex />
-        </BrowserRouter>
-      )
-      expect(screen.getByText(/apartment listings/i)).toBeInTheDocument();
-      })
-   })
-  
+  render(
+    <BrowserRouter>
+      <ApartmentIndex />
+    </BrowserRouter>
+  )
+  it("renders without crashing", () => {
+    expect(screen.getByText(/apartment listings/i)).toBeInTheDocument();
+  })
+
+  it("renders Card",()=>{
+    render(
+      <Card/>
+    )
+   expect(Card).toBeDefined()
+  })
+})
